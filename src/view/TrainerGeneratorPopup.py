05c1b41f7132937c08d15111c00b34f2bb22031e
baptistegosselet <baptiste.gosselet.outlook.fr>
2023-01-22 15:06:15 +0100

    Choix team aléatoire

--- src/view/TrainerGeneratorPopup.py
+++ src/view/TrainerGeneratorPopup.py
@@ -1,3 +1,4 @@
+from random import randint
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import Qt
 
@@ -5,6 +6,9 @@ class TrainerGeneratorPopup(QDialog):
     
     def __init__(self, saisieNom_J1, saisieNom_J2, caseAI, combos_J1, combos_J2):
         super().__init__()
+
+        self.combos_J1 = combos_J1
+        self.combos_J2 = combos_J2
 
         self.setWindowTitle("Pokemon Battle System")
         self.setWindowModality(Qt.WindowModality.ApplicationModal)
@@ -25,6 +29,15 @@ class TrainerGeneratorPopup(QDialog):
             choice_layout_J1.addWidget(combos_J1[i])
             choice_layout_J2.addWidget(combos_J2[i])
         
+        shuffle_button_1 = QPushButton("Aléatoire")
+        shuffle_button_1.clicked.connect(self.onClick_rand1)
+        choice_layout_J1.addWidget(shuffle_button_1)
+
+        shuffle_button_2 = QPushButton("Aléatoire")
+        shuffle_button_2.clicked.connect(self.onClick_rand2)
+        choice_layout_J2.addWidget(shuffle_button_2)
+
+
         globalLayout.addLayout(choice_layout_J1, 2, 0)
         globalLayout.addLayout(choice_layout_J2, 2, 1)
 
@@ -35,6 +48,15 @@ class TrainerGeneratorPopup(QDialog):
         self.setLayout(globalLayout)
 
         self.exec()
+
+    def onClick_rand1(self):
+        for combo in self.combos_J1:
+            combo.setCurrentIndex(randint(0,combo.count()-1))
+
+    def onClick_rand2(self):
+        for combo in self.combos_J2:
+            combo.setCurrentIndex(randint(0,combo.count()-1))
+
 
     def onClick_ok(self):
         self.close()
