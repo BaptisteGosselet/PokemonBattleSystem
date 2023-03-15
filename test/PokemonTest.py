a3f695a1ea1dcc29418f9cedefec1e60e6be4ef5
Baptiste Gosselet <baptiste.gosselet.etu@univ-lille.fr>
2023-03-15 09:54:52 +0100

    Signature Test Classe Pokemon

--- test/PokemonTest.py
+++ test/PokemonTest.py
@@ -1,167 +1,48 @@
-def testPkm1():
+import testUtils
+
+def getPourcentageHP_Test():
+    #return ceil((self.current_HP / self.MAX_HP) * 100)
     return False
 
-def testPkm2():
-    return True
+def getCurrentHP_Test():
+    return False
 
+def getMaxHP_Test():
+    return False
 
-"""
-class Pokemon : 
+def getAtkStat_Test():
+    #quand brûlé
+    return False
 
-    def __init__(self, name, type1, type2,  hp, atkStat, defStat, spaStat, spdStat, speStat, move1, move2, move3, move4):
-    
-        self.name = name 
-        self.type1 = type1
-        self.type2 = type2
-        
-        self.MAX_HP = 2*hp+110
-        self.current_HP = self.MAX_HP
-        self.atkStat = atkStat
-        self.defStat = defStat
-        self.spaStat = spaStat
-        self.spdStat = spdStat
-        self.speStat = speStat
+def getSpaStat_Test():
+    #quand freeze
+    return False
 
-        self.atkModif = 0
-        self.defModif = 0
-        self.spaModif = 0
-        self.spdModif = 0
-        self.speModif = 0
+def getSpeStat_Test():
+    #quand paralysé
+    return False
 
-        self.move1 = move1
-        self.move2 = move2
-        self.move3 = move3
-        self.move4 = move4
+def applyStatus_Test():
+    #si déjà status ne rien faire
+    return False
 
-        self.status = None
+def modifAtk_Test():
+    #voir getatk ensuite
+    return False
 
-        self.trainer = None
+def heal_Test():
+    return False
 
-        self.isKo = False
+def applyDamage_Test():
+    #infliger des dégats et voir si ko
+    return False
 
+def canParalyse_Test():
+    #type electrik
+    return False
 
-    def getName(self)->str:
-        return self.name
+def canPoison_Test():
+    return False
 
-    def getMove1(self)->Move:
-        return self.move1
-
-    def getMove2(self)->Move:
-        return self.move2
-
-    def getMove3(self)->Move:
-        return self.move3
-
-    def getMove4(self)->Move:
-        return self.move4
-
-    def getPourcentageHP(self)->int:
-        return ceil((self.current_HP / self.MAX_HP) * 100)
-
-    def getCurrentHP(self)->int:
-        return self.current_HP
-
-    def getMaxHP(self)->int:
-        return self.MAX_HP
-
-    def getAtkStat(self)->int:
-        if(type(self.status)==BurnStatus):
-            return int(self.atkStat // 2 + (self.atkModif*25/100))
-        return int(self.atkStat + (self.atkModif*25/100))
-
-    def getDefStat(self)->int:
-        return int(self.defStat + (self.defModif*25/100))
-
-    def getSpaStat(self)->int:
-        if(type(self.status)==FreezeStatus):
-            return int(self.spaStat // 2 + (self.spaModif*25/100))
-        return int(self.spaStat + (self.spaModif*25/100))
-
-    def getSpdStat(self)->int:
-        return int(self.spdStat + (self.spdModif*25/100))
-
-    def getSpeStat(self)->int:
-        if(type(self.status)==ParalysisStatus):
-            return int(self.speStat // 4 + (self.speModif*25/100))
-        return int(self.speStat + (self.speModif*25/100))
-
-    def getIsKo(self)->bool:
-        return self.isKo
-
-    def getType1(self)->TypePkmn:
-        return self.type1
-
-    def getType2(self)->TypePkmn:
-        return self.type2
-
-    def setTrainer(self, trainer)->None:
-        self.trainer = trainer
-
-    def getTrainer(self):
-        return self.trainer
-
-    def setStatus(self, status):
-        self.status = status
-
-    def getStatus(self)->Status:
-        return self.status
-
-    def applyStatus(self):
-        if(self.status != None):
-            self.status.applyStatus(self)
-
-    def modifAtk(self, n:int):
-        self.atkModif += n
-
-    def modifDef(self, n:int):
-        self.defModif += n  
-
-    def modifSpa(self, n:int):
-        self.spaModif += n  
-
-    def modifSpd(self, n:int):
-        self.spdModif += n  
-
-    def modifSpe(self, n:int):
-        self.speModif += n  
-
-    def resetModif(self):
-        self.atkModif = 0
-        self.defModif = 0
-        self.spaModif = 0
-        self.spdModif = 0
-        self.speModif = 0
-
-    def heal(self, pv)->None:
-        if(not self.isKo):
-            self.current_HP += pv
-            if(self.current_HP > self.MAX_HP):
-                self.current_HP = self.MAX_HP
-
-    def applyDamage(self, damage)->None:
-        #Apply a number of damage to the HP, if current HPs are lower to 0 then the pokemon is K.O.
-        self.current_HP -= damage
-        if(self.current_HP < 0):
-            self.current_HP = 0
-            self.isKo = True
-
-    def canParalyse(self)->bool:
-        #Check if the pokemon can be paralysed (electrik type pokemon can't be paralysed)
-        #@return if the status is possible on this pokemon
-        if(self.type1.getName() == "Electrik"):
-            return False
-        elif(self.type2 != None):
-            if(self.type2.getName() == "Electrik"):
-                return False
-        return True
-
-    def canPoison(self)->bool:
-        #Check if the pokemon can be poisonned (poison type pokemon can't be poisonned)
-        #@return if the status is possible on this pokemon
-        if(self.type1.getName() == "Poison"):
-            return False
-        elif(self.type2 != None):
-            if(self.type2.getName() == "Poison"):
-                return False
-        return True
-"""
+def canFreeze_Test():
+    return False
--- test/mainTest.py
+++ test/mainTest.py
@@ -4,25 +4,27 @@ import PokemonTest
 
 
 class color:
+
     OK = '\033[92m' #GREEN
     FAIL = '\033[91m' #RED
     RESET = '\033[0m' #RESET COLOR
+    TITLE = '\033[94m' #BLUE
 
 def test_a_function(fun:bool):
     """
     Test a function and display if the test success
     @param fun : the function to test
     """
-    line = ""
-    status = ""
+    line = "\t- "
+    status = "\t\t\t"
 
     res = fun()
     if(res):
-        line = color.OK
-        status = "OK"
+        line += color.OK
+        status += "OK"
     else:
-        line = color.FAIL
-        status = "FAIL"
+        line += color.FAIL
+        status += "FAIL"
 
     line += fun.__name__
     line += " : "
@@ -36,10 +38,12 @@ def test_a_module(mod):
     test all functions of a module
     @param mod : the module to test
     """
+    print(color.TITLE,mod.__name__, color.RESET)
     all_functions = inspect.getmembers(mod, inspect.isfunction)
     for key, value in all_functions:
         if str(inspect.signature(value)) == "()":
             test_a_function(value)
+    print("\n")
 
 def launch_test():
     """
@@ -50,10 +54,6 @@ def launch_test():
 
 
 
-"""
-    for fun in dir(PokemonTest) :
-        test_a_function(fun)
-"""
     #Pokemon
     #PokemonFactory
     #TypePkmn
